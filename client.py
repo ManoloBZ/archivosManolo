e395e28026e8c1c1b13e4ad4542416005c3dbcd6
ManoloBZ <66225626+ManoloBZ@users.noreply.github.com>
2020-11-18 11:40:52 -0500

    Update client.py

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.0.105",8080))
+s.connect(("179.14.251.47",8080))
 
 while True:
     x=input("Ingrese 1 o 0 para conmutar")
